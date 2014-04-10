4e16704fe6edd965a8cbc3388e3b3a3520ea5cfa
rksipes <rksipes@eagles.ewu.edu>
2014-04-10 12:27:36 -0700

    first commit

--- src/MineField.java
+++ src/MineField.java
@@ -67,6 +67,42 @@ public class MineField{
 		return NOT_MINE;
 	}//plantMine()
 	
+	public char[][] detectMines() {
+		int rows = this.getRows();
+		int cols = this.getColumns();
+		char MINE = this.getMINE();
+		char[][] theField = this.getField();
+		char[][] sweptField = new char[rows][cols];
+		int mineCount;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				mineCount = 0;
+				if (theField[i][j] != MINE) {
+					if (i - 1 >= 0 && theField[i-1][j] == MINE) //up
+						mineCount++;
+					if (i + 1 < rows && theField[i+1][j] == MINE) //down
+						mineCount++;
+					if (j - 1 >= 0 && theField[i][j-1] == MINE) //left
+						mineCount++;
+					if (j + 1 < cols && theField[i][j+1] == MINE) //right
+						mineCount++;
+					if (j + 1 < cols && i + 1 < rows && theField[i+1][j+1] == MINE) //lower right
+						mineCount++;
+					if (j + 1 < cols && i - 1 >= 0 && theField[i-1][j+1] == MINE) //upper right
+						mineCount++;
+					if (j - 1 >= 0 && i + 1 < rows && theField[i+1][j-1] == MINE) //lower left
+						mineCount++;
+					if (j - 1 >= 0 && i - 1 >= 0 && theField[i-1][j-1] == MINE) //upper left
+						mineCount++;
+					sweptField[i][j] = Character.forDigit(mineCount, 10);
+				} else {
+					sweptField[i][j] = MINE;
+				}
+			}
+		}
+		return sweptField;
+	}//detectMines()
+	
 	public String toString() {
 		String str = "";
 		for(char[] row : this.mineField) {
--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,55 +1,54 @@
-import java.util.Scanner;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MineSweeper {
 
 	public static void main(String[] args) {
-		ArrayList fields = retrieveFields();
+		ArrayList<MineField> fields = retrieveFields();
 		printSweptFields(fields);
 	}
-
-	private static ArrayList retrieveFields() {
-		int[] dimensions = getFieldDimensions();
-		ArrayList fields = new ArrayList();
-		while (dimensions[0] != 0){
-			MineField field = new MineField(dimensions[0], dimensions[1]);
-			fillField(field);
-			fields.add(field);
-			dimensions = getFieldDimensions();
+	
+	private static ArrayList<MineField> retrieveFields() {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(System.in/*new File("TestCases.txt")*/);
+		} catch (Exception ex) {
+			System.out.print(ex);
 		}
+		ArrayList<MineField> fields = new ArrayList<MineField>();
+		while(sc.hasNext()) {
+			int rows = sc.nextInt();
+			int cols = sc.nextInt();
+			if (rows != 0 && cols != 0) {
+				sc.nextLine();
+				int rowNum = 0;
+				MineField field = new MineField(rows, cols);	
+				while (rowNum < rows) {			
+					char[] fieldRow = sc.nextLine().toCharArray();
+					field.layMines(rowNum, fieldRow);
+					rowNum++;
+				}
+				fields.add(field);
+			}			
+		}
+		sc.close();
 		return fields;
 	}//retrieveFields()
-
-	private static int[]  getFieldDimensions() {
-		Scanner sc = new Scanner(System.in); 
-		System.out.print("Enter the number of rows & columns separated by a space: ");
-		int[] dimensions = new int[2];
-		dimensions[0] = sc.nextInt();
-		dimensions[1] = sc.nextInt();
-		return dimensions;
-	}//getFieldDimensions()
-
-	private static void fillField(MineField field) {
-		int rows = field.getRows();
-		int index = 0;
-		Scanner sc = new Scanner(System.in);
-		while (rows > 0) {
-			System.out.print("Enter the mine field below: " + index + ": ");
-			String fieldRow = sc.nextLine();
-			field.layMines(index, fieldRow.toCharArray());
-			rows--;
-			index++;
-		}
-	}//fillField()
 	
-	private static void printSweptFields(ArrayList fields) {
-		int count = 1;
-		for (Object field : fields) {
-			MineField theField = (MineField)field;
-			//System.out.println(theField.toString());
-			SweptField sweptField = new SweptField(theField);
-			System.out.print("Field #" + count + ":\n" + sweptField.toString());
-			count++;
+	private static void printSweptFields(ArrayList<MineField> fields) {
+		int fieldNum = 1;
+		for (MineField field: fields) {
+			char[][] sweptField = field.detectMines();
+			System.out.println("Field #" + fieldNum + ":");
+			for(char[] row : sweptField) {
+				for (char c : row) {
+					System.out.print(c);
+				}
+				System.out.println();
+			}
+			fieldNum++;
 		}
 	}//printSweptFields()
+
 }
